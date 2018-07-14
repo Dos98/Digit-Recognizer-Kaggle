a15fa874830eac3d387aa807335d5119c9d2acc8
Dos98 <ayushdosaj2313@gmail.com>
2018-07-15 00:25:18 +0530

    LeNet

--- LeNet with dropout final.py
+++ LeNet with dropout final.py
@@ -1,11 +1,11 @@
-# import the necessary packages
+# import the packages
 from keras import backend as K
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
-from keras.layers.core import Dense
+from keras.layers.core import Dense, Dropout
 from keras.datasets import mnist
 from keras.utils import np_utils
 from keras.optimizers import SGD, RMSprop, Adam
@@ -15,25 +15,33 @@ import matplotlib.pyplot as plt
 
 np.random.seed(1671)  # for reproducibility
 
-#define the convnet 
+#define the convnet class
 class LeNet:
 	@staticmethod
 	def build(input_shape, classes):
 		model = Sequential()
-		# CONV => RELU => POOL 
-		model.add(Conv2D(20, kernel_size=5, padding="same",
+		# CONV => RELU => POOL => DROPOUT
+		model.add(Conv2D(32, kernel_size=5, padding="same",
 			input_shape=input_shape))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-		# CONV => RELU => POOL
-		model.add(Conv2D(50, kernel_size=5, padding="same"))
+		model.add(Dropout(0.75))
+		# CONV => RELU => POOL => DROPOUT
+		model.add(Conv2D(64, kernel_size=5, padding="same"))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+		model.add(Dropout(0.75))
+		# CONV => RELU 
+		model.add(Conv2D(128, kernel_size=5, padding="same"))
+		model.add(Activation("relu"))
 		# Flatten => RELU layers
 		model.add(Flatten())
-		model.add(Dense(500))
+		model.add(Dense(2048))
 		model.add(Activation("relu"))
- 
+		model.add(Dropout(0.5))
+		model.add(Dense(625))
+		model.add(Activation("relu"))
+		model.add(Dropout(0.5))
 		# a softmax classifier
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
@@ -41,37 +49,33 @@ class LeNet:
 		return model
 
 # network and training
-NB_EPOCH = 20
+NB_EPOCH = 100
 BATCH_SIZE = 128
 VERBOSE = 1
 OPTIMIZER = Adam()
 VALIDATION_SPLIT=0.2
 
-IMG_ROWS, IMG_COLS = 28, 28 # input image dimensions
-NB_CLASSES = 10  # number of outputs = number of digits
+IMG_ROWS, IMG_COLS = 28, 28 
+NB_CLASSES = 10  
 INPUT_SHAPE = (1, IMG_ROWS, IMG_COLS)
 
-# data: shuffled and split between train and test sets
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-train = pd.read_csv('/home/ayush/Desktop/all/train.csv')
-X_train = train.ix[:,1:].values.astype('float32')
+train = pd.read_csv('../input/train.csv')
+X_train = train.ix[:,1:].values.astype('float32') #consider as float
 y_train = train.ix[:,0].values.astype('int32')
-X_test = pd.read_csv('/home/ayush/Desktop/all/test.csv').values.astype('float32')
-# print(train_images.shape, train_labels.shape, test_images.shape)
+X_test = pd.read_csv('../input/test.csv').values.astype('float32')
+
 
 K.set_image_dim_ordering("th")
 
-# consider them as float and normalize
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
+
 X_train /= 255 
 X_test /= 255  
 
 # we need a 60K x [1 x 28 x 28] shape as input to the CONVNET
 # X_train = X_train[:, np.newaxis, :, :]
 # X_test = X_test[:, np.newaxis, :, :]
-X_train = X_train.values.reshape(-1,28,28,1)
-X_test = X_test.values.reshape(-1,28,28,1)
+X_train = X_train.reshape(-1,1,28,28)
+X_test = X_test.reshape(-1,1,28,28)
 
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
@@ -82,9 +86,7 @@ print(y_train.shape[0], 'train outputs')
 
 print(X_train.shape)
 
-# y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 
-# initialize the optimizer and model
 model = LeNet.build(input_shape=INPUT_SHAPE, classes=NB_CLASSES)
 model.compile(loss="categorical_crossentropy", optimizer=OPTIMIZER,
 	metrics=["accuracy"])
@@ -119,7 +121,7 @@ print("fitting done")
 # plt.show()
 
 # Generate prediction for test set
-predictions = model.predict_classes(test_images, verbose=0)
-
+predictions = model.predict_classes(X_test, verbose=0)
+print(predictions)
 submissions=pd.DataFrame({"ImageId": list(range(1,len(predictions)+1)),"Label": predictions})
-submissions.to_csv("mnist_predictions.csv", index=False, header=True)
+submissions.to_csv("lenet_with_dropout_75_percent_mnist_predictions.csv", index=False, header=True)
